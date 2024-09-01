8276403eddc33f17618fe6811a097103bad80a91
Federico Ramos <federico.ramos@wazuh.com>
2024-09-01 15:00:26 -0300

    Agregado de documentación.
    Se agrego explicación del ejercicio al README.md al igual
    que se agregaron docstrings para docker_generator package.

--- scripts/docker_generator/services_yaml.py
+++ scripts/docker_generator/services_yaml.py
@@ -1,10 +1,26 @@
 import textwrap
 
 class ServicesDefinition:
+    """
+    A class to define and generate YAML configurations for services.
+    """
+
     def __init__(self, log_level: str = "DEBUG"):
+        """
+        Initializes the ServicesDefinition with a specified logging level.
+
+        Args:
+            log_level (str): The logging level for the service. Defaults to "DEBUG".
+        """
         self._log_level = log_level
 
     def get_server_definition(self) -> str:
+        """
+        Generates the YAML configuration for the server service.
+
+        Returns:
+            str: The YAML configuration for the server service.
+        """
         return (
             f"  server:\n"
             f"    container_name: server\n"
@@ -18,6 +34,12 @@ class ServicesDefinition:
         )
 
     def get_networks_definition(self) -> str:
+        """
+        Generates the YAML configuration for the networks.
+
+        Returns:
+            str: The YAML configuration for the networks.
+        """
         networks_definition = textwrap.dedent(f"""
             networks:
               testing_net:
@@ -30,6 +52,15 @@ class ServicesDefinition:
         return networks_definition
 
     def get_yaml(self, content: str) -> str:
+        """
+        Generates the complete YAML configuration including services and networks.
+
+        Args:
+            content (str): Additional YAML content to be included.
+
+        Returns:
+            str: The complete YAML configuration.
+        """
         yaml_content = (
             "name: tp0\n"
             "services:\n"
